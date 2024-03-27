159597becaf224e1f7b7a7a0e1c3ac7c63ea57d3
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-03-27 11:40:35 +0000

    creating features data

--- src/Data.js
+++ src/Data.js
@@ -60,6 +60,13 @@ import imgSomwarpetMobile from "./assets/stories/mobile/somwarpet.jpg";
 import imgLandOfDreamsDesktop from "./assets/stories/desktop/land-of-dreams.jpg";
 import imgLandOfDreamsMobile from "./assets/stories/mobile/land-of-dreams.jpg";
 
+import imgResponsive from "./assets/features/desktop/responsive.svg";
+import imgNoLimit from "./assets/features/desktop/no-limit.svg";
+import imgEmbed from "./assets/features/desktop/embed.svg";
+import imgCustomDomain from "./assets/features/desktop/custom-domain.svg";
+import imgBoostExposure from "./assets/features/desktop/boost-exposure.svg";
+import imgDragDrop from "./assets/features/desktop/drag-drop.svg";
+
 export const infoComponentsData = [
   {
     id: 1,
@@ -256,5 +263,44 @@ export const storiesData = [
       desktop: imgLandOfDreamsDesktop,
       mobile: imgLandOfDreamsMobile,
     },
+  },
+];
+
+export const featuresData = [
+  {
+    id: 1,
+    image: imgResponsive,
+    title: "100% Responsive",
+    text: "No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.",
+  },
+  {
+    id: 2,
+    image: imgNoLimit,
+    title: "No Photo Upload Limit",
+    text: "Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.",
+  },
+  {
+    id: 3,
+    image: imgEmbed,
+    title: "Available to Embed",
+    text: "Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. ",
+  },
+  {
+    id: 4,
+    image: imgCustomDomain,
+    title: "Custom Domain",
+    text: "With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding!",
+  },
+  {
+    id: 5,
+    image: imgBoostExposure,
+    title: "Boost Your Exposure",
+    text: "Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list.",
+  },
+  {
+    id: 6,
+    image: imgDragDrop,
+    title: "Drag & Drop Image",
+    text: "Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories.",
   },
 ];
