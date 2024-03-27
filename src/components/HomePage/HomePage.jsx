9c4828bb6455168052db5daebf919a7408212b0d
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-03-27 18:35:22 +0000

    update style for features

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -178,7 +178,10 @@ const FeatureComponent = ({ obj }) => {
   const { title, text, image } = obj;
   return (
     <div className="feature__component">
-      <img src={image} alt="feature icon" className="feature__img" />
+      <div className="feature__component__image-wrapper">
+        <img src={image} alt="feature icon" className="feature__img" />
+      </div>
+
       <h4 className="feature__title">{title}</h4>
       <p className="feature__text">{text}</p>
     </div>
