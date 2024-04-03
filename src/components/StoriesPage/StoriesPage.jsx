5ab8be73afb0edf9ef964f62d8ed1b4e201eb91a
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-03 12:26:17 +0100

    stories page hero jsx

--- src/components/StoriesPage/StoriesPage.jsx
+++ src/components/StoriesPage/StoriesPage.jsx
@@ -27,6 +27,30 @@ const StoriesHero = ({ data }) => {
           Last month&apos;s featured story
         </h4>
         <h1 className="hero__heading-main">Hazy full moon of appalachia</h1>
+        <div className="date__box">
+          <p className="hero__text">
+            March 2nd 202<strong> by John Appleseed</strong>
+          </p>
+        </div>
+        <p className="hero__text">
+          The dissected plateau area, while not actually made up of geological
+          mountains, is popularly called &quot;mountains&quot; mountains
+          especially in eastern Kentucky and West Virginia, and while the ridges
+          are not high, the terrain is extremely rugged.
+        </p>
+        <div className="link--box">
+          <p className="hero__read">Read story</p>
+          <svg
+            width="42"
+            height="14"
+            viewBox="0 0 42 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 7H41.864" stroke="white" />
+            <path d="M35.4282 1L41.4282 7L35.4282 13" stroke="white" />
+          </svg>
+        </div>
       </div>
     </div>
   );
