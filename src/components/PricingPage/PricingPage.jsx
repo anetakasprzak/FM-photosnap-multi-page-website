073be10dcef03d7f4e80440aa88f54d437e307f7
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-09 18:11:46 +0100

    cards media queries

--- src/components/PricingPage/PricingPage.jsx
+++ src/components/PricingPage/PricingPage.jsx
@@ -19,10 +19,14 @@ const PricingCard = ({ obj }) => {
   const { title, text, priceYear, priceMonth, id } = obj;
   return (
     <div className={`pricing__card pricing__card--${id}`}>
-      <h4 className="card__heading">{title}</h4>
-      <p className="text card__text">{text}</p>
-      <div className="card__price">{priceMonth}</div>
-      <span className="text card__text">per month</span>
+      <div className="card__text--box">
+        <h4 className="card__heading">{title}</h4>
+        <p className="text card__text">{text}</p>
+      </div>
+      <div className="card__price--box">
+        <div className="card__price">{priceMonth}</div>
+        <span className="text card__text">per month</span>
+      </div>
       {/* <div className="">{priceYear}</div> */}
       <button className={`card__btn card__btn--${id}`}>Pick plan</button>
     </div>
