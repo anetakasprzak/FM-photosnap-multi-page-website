4be396d8e4e39d16cbd7f3a78af0f76d14b080c8
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-09 19:14:02 +0100

    pricing toggle input

--- src/components/PricingPage/PricingPage.jsx
+++ src/components/PricingPage/PricingPage.jsx
@@ -5,6 +5,7 @@ import { ctaImg } from "../../Data";
 
 import CtaComponent from "../CtaComponent/CtaComponent";
 import FooterSection from "../Footer/Footer";
+import { useState } from "react";
 
 const PricingPage = () => {
   return (
@@ -18,16 +19,41 @@ const PricingPage = () => {
 };
 
 const CardsSection = ({ data }) => {
+  const [isMonthly, setIsMonthly] = useState(true);
+
   return (
-    <section className="cards__section">
-      {data.map((obj) => {
-        return <PricingCard obj={obj} key={obj.id} />;
-      })}
-    </section>
+    <div className="pricing__box">
+      <div className="toggle__box">
+        <span
+          className={`text ${
+            isMonthly ? "toggle__text--black" : "toggle__text--grey"
+          } `}
+        >
+          Monthly
+        </span>
+        <label className="switch">
+          <input type="checkbox" onChange={() => setIsMonthly(!isMonthly)} />
+          <span className="slider round"></span>
+        </label>
+        <span
+          className={`text ${
+            !isMonthly ? "toggle__text--black" : "toggle__text--grey"
+          } `}
+        >
+          Yearly
+        </span>
+      </div>
+
+      <section className="cards__section">
+        {data.map((obj) => {
+          return <PricingCard isMonthly={isMonthly} obj={obj} key={obj.id} />;
+        })}
+      </section>
+    </div>
   );
 };
 
-const PricingCard = ({ obj }) => {
+const PricingCard = ({ obj, isMonthly }) => {
   const { title, text, priceYear, priceMonth, id } = obj;
   return (
     <div className={`pricing__card pricing__card--${id}`}>
@@ -36,10 +62,20 @@ const PricingCard = ({ obj }) => {
         <p className="text card__text">{text}</p>
       </div>
       <div className="card__price--box">
-        <div className="card__price">{priceMonth}</div>
-        <span className="text card__text">per month</span>
+        {isMonthly && (
+          <>
+            <div className="card__price">{priceMonth}</div>
+            <span className="text card__text">per month</span>{" "}
+          </>
+        )}
+
+        {!isMonthly && (
+          <>
+            <div className="card__price">{priceYear}</div>
+            <span className="text card__text">per year</span>
+          </>
+        )}
       </div>
-      {/* <div className="">{priceYear}</div> */}
       <button className={`card__btn card__btn--${id}`}>Pick plan</button>
     </div>
   );
