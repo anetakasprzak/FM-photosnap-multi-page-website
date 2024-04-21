9ea9009b1ee4c68cd9953e4f36d3f9d05e923ffe
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-04-21 10:28:05 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fm-photosnap-multi-page-website/",
+  base: "https://anetakasprzak.github.io/fm-photosnap-multi-page-website/",
 });
